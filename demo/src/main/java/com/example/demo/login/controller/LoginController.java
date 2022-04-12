9e899b852c85e082c44f5666d552e2d7d0d0421d
Dongcheul SH <abcdefg619@naver.com>
2022-04-13 08:59:27 +0900

    feat : 로그인 필터, 컨트롤러 추가

---
+++ demo/src/main/java/com/example/demo/login/controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.example.demo.login.controller;
+
+import com.example.demo.common.utility.JwtUtility;
+import com.google.gson.JsonObject;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.HashMap;
+
+@Slf4j
+public class LoginController {
+
+    @PostMapping("/login")
+    public String makeJwt(){
+        JsonObject jo = new JsonObject();
+        jo.addProperty("jwt", JwtUtility.makeJwtToken());
+        return jo.toString();
+    }
+
+}
