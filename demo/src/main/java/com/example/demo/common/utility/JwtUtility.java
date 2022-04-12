e68d9f59ab632a8c1b1ee02953d0504d5c4de57b
Dongcheul SH <abcdefg619@naver.com>
2022-04-13 08:58:18 +0900

    init : 디렉토리 구조 수정

--- demo/src/main/java/com/example/demo/common/utility/JwtUtility.java
+++ demo/src/main/java/com/example/demo/common/utility/JwtUtility.java
@@ -1,22 +1,14 @@
-package com.example.demo.Test;
+package com.example.demo.common.utility;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 import java.time.Duration;
 import java.util.Date;
-import java.util.UUID;
 
-@Slf4j
-public class JwtFilter implements Filter {
-
+public class JwtUtility {
     public static String makeJwtToken() {
         Date now = new Date();
 
@@ -50,30 +42,5 @@ public class JwtFilter implements Filter {
 
     private static String extractToken(String authorizationHeader) {
         return authorizationHeader.substring("Bearer ".length());
-    }
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        log.info("Jwt 필터 생성");
-    }
-
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-
-        String authorizationHeader = httpRequest.getHeader(HttpHeaders.AUTHORIZATION);
-        Claims claims = parseJwtToken(authorizationHeader);
-        String parsedData = (String)claims.get("id");
-        log.info("parsedData : {}",parsedData);
-        if(!parsedData.equals("신동철")){ // 파싱된 값의 id 에 매칭되는 value 가 신동철이 아니라면 종료.
-            return;
-        }
-
-        chain.doFilter(request, response); // 신동철이란 값이 제대로 나왔따면 진행
-    }
-
-    @Override
-    public void destroy() {
-        log.info("Jwt 필터 종료");
     }
 }
